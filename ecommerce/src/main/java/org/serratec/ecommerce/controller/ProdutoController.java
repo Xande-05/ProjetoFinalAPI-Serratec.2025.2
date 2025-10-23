f215fbf99dc47c195e4b9c89b67913758460e138
dianasribeiro <dianasribeiroti@gmail.com>
2025-10-23 20:10:35 -0300

    nova atualizacao

--- ecommerce/src/main/java/org/serratec/ecommerce/controller/ProdutoController.java
+++ ecommerce/src/main/java/org/serratec/ecommerce/controller/ProdutoController.java
@@ -1,5 +1,56 @@
 package org.serratec.ecommerce.controller;
 
+import java.util.List;
+
+import org.serratec.ecommerce.dto.ProdutoRequestDTO;
+import org.serratec.ecommerce.dto.ProdutoResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
 
+    private static final HttpStatusCode CREATED = null;
+    
+	@Autowired
+    private ProdutoService produtoService;
+    
+    // Produto insert (POST)
+    @PostMapping
+    public ResponseEntity<ProdutoResponseDTO> inserir(@RequestBody ProdutoRequestDTO dto) {
+        ProdutoResponseDTO produtoCriado = produtoService.inserir(dto);
+        return ResponseEntity.status(CREATED).body(produtoCriado); 
+    }
+    
+    // Produto  edit (PUT)
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDTO> editar(@PathVariable Long id, @RequestBody ProdutoRequestDTO dto) {
+        ProdutoResponseDTO produtoAtualizado = produtoService.editar(id, dto);
+        return ResponseEntity.ok(produtoAtualizado);
+    }
+    
+    //  Produto list (GET)
+    @GetMapping
+    public ResponseEntity<List<ProdutoResponseDTO>> listar() {
+        List<ProdutoResponseDTO> produtos = produtoService.listarTodos();
+        return ResponseEntity.ok(produtos); 
+    }
+    
+    // Produto delete (DELETE)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+		produtoService.deletar(id);
+		return ResponseEntity.noContent().build(); 
+	}
 }
