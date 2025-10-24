d480f5f64689146534dc08be9e424c5e0ecf61d3
JPMASTAMBANI <canaldomassambani@gmail.com>
2025-10-23 23:49:27 -0300

    Corrigindo erros para finalizacao

--- ecommerce/src/main/java/org/serratec/ecommerce/entity/Perfil.java
+++ ecommerce/src/main/java/org/serratec/ecommerce/entity/Perfil.java
@@ -1,12 +1,13 @@
 
 package org.serratec.ecommerce.entity;
 
-import org.springframework.data.annotation.Id;
+
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
 
